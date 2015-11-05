c05e8dc9fc0fb474735a6b5f8948c3a49b3a638f
Cécilia Lejeune <cecilia.lejeu@gmail.com>
2015-11-05 21:57:40 +0100

    * update of the strangeIoC library * Cleaning of the example files to start on a clean base

--- Assets/Scripts/main/MainContext.cs
+++ Assets/Scripts/main/MainContext.cs
@@ -1,23 +1,5 @@
-/*
- * Copyright 2013 ThirdMotion, Inc.
- *
- *	Licensed under the Apache License, Version 2.0 (the "License");
- *	you may not use this file except in compliance with the License.
- *	You may obtain a copy of the License at
- *
- *		http://www.apache.org/licenses/LICENSE-2.0
- *
- *		Unless required by applicable law or agreed to in writing, software
- *		distributed under the License is distributed on an "AS IS" BASIS,
- *		WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *		See the License for the specific language governing permissions and
- *		limitations under the License.
- */
-
 /// MainContext maps the Context for the top-level component.
-/// ===========
-/// I'm assuming here that you've already gone through myfirstproject, or that
-/// you're experienced with strange.
+
 
 using System;
 using UnityEngine;
@@ -25,9 +7,9 @@ using strange.extensions.context.api;
 using strange.extensions.context.impl;
 using strange.extensions.dispatcher.eventdispatcher.api;
 using strange.extensions.dispatcher.eventdispatcher.impl;
-using strange.examples.multiplecontexts.game;
+using tikal.game;
 
-namespace strange.examples.multiplecontexts.main
+namespace tikal.main
 {
 	public class MainContext : MVCSContext
 	{

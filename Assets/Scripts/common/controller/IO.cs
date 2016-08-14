02f2a9ff13079ea4751535568c028b18ab6f20b5
Cécilia Lejeune <cecilia.lejeu@gmail.com>
2016-08-14 18:31:09 +0200

    Update to the latest unity version

---
+++ Assets/Scripts/common/controller/IO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using strange.extensions.command.impl;
+using System.IO;
+using strange.extensions.context.api;
+
+namespace tikal.common
+{
+	public enum BYTE_ORDER
+	{
+		Mac,
+		Win
+	}
+	public static class IO{
+
+		//public static float[][] readHeightmap(string path) {
+
+			/*Texture2D heightmap = null;
+			byte[] fileData;
+			
+			if (File.Exists(path))     {
+				//fileData = File.ReadAllBytes(path);
+				heightmap = new Texture2D(2, 2);
+				heightmap.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+			}
+			for (int y = 0; y < heightmap.height;y++){ 
+				for (int x = 0; x < heightmap.width;x++){
+
+					float color = heightmap.GetPixel(x, y);
+
+					//If pixel data is between 0 and 5 (black) or between 250 and 255 (white) => it's height information	
+					if ((color>=0 && color<=5)||(color >=250 && color <= 255)){ 
+
+					}else{
+						//grey
+					}
+
+				}
+			}*/
+
+
+		//}
+	}
+}
+

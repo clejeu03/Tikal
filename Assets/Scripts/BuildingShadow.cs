fc9bf98ef6c02d7cfa06029a24e6e92d4322e69a
Cécilia Lejeune <cecilia.lejeu@gmail.com>
2016-08-15 18:14:42 +0200

    Grounded the shadow and the improved the mouse raycasting to position the building

--- Assets/Scripts/BuildingShadow.cs
+++ Assets/Scripts/BuildingShadow.cs
@@ -8,9 +8,11 @@ namespace tikal.game
 
 		private string m_message;
 		private bool m_okForConstruction = true;
+		private GameObject goTerrain;
 
 		private Material m_blueprintOK;
 		private Material m_blueprintNO;
+		private float distanceToGround = 100f;
 
 		public delegate void ConstructBuilding();
 		public static event ConstructBuilding OnConstructBuilding;
@@ -21,21 +23,28 @@ namespace tikal.game
 		void Start(){
 			m_blueprintOK = (Material)Resources.Load ("Materials/BuildingOKBlueprint");
 			m_blueprintNO = (Material)Resources.Load ("Materials/BuildingNOBlueprint");
-
+			goTerrain = GameObject.FindGameObjectWithTag("Terrain");
 			GetComponent<MeshRenderer>().material = m_blueprintNO;
 		}
 
 		void Update(){
-			this.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 50));
-
-			//Change the material color //
+			// ------------------ POSITION -------------------- //
+			Ray ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 50));
+			RaycastHit hit;
+			if(goTerrain != null){
+				if (goTerrain.GetComponent<Collider>().Raycast (ray, out hit, Mathf.Infinity)) {
+					this.transform.position = hit.point;
+				}
+			}
+				
+			// ----------------- COLOR FEEDBACK ---------------- //
 			if(m_okForConstruction){
 				GetComponent<MeshRenderer>().material = m_blueprintOK;
 			}else{
 				GetComponent<MeshRenderer>().material = m_blueprintNO;
 			}
 
-			// MOUSE INPUTS //
+			// -------------- MOUSE INPUTS --------------------- //
 			if (Input.GetMouseButtonDown (1)) {
 				if (OnCancelConstruction != null)
 					OnCancelConstruction ();
@@ -50,12 +59,10 @@ namespace tikal.game
 
 		void OnTriggerEnter(Collider col) {
 			m_okForConstruction = false;
-
 		}
 
 		void OnTriggerExit(Collider col) {
 			m_okForConstruction = true;
-
 		}
 	}
 }
